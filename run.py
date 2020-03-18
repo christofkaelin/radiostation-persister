24715c233835265b5171903f3b2d68a39f9ede2e
Kälin Christof iakaelin <christof.kaelin@stud.hslu.ch>
2020-03-18 12:57:53 +0100

    Implement functionality to load parsed data into DB

--- run.py
+++ run.py
@@ -1,22 +1,10 @@
-import urllib.request, json
-import mysql.connector as mariadb
+import os
 import yaml
+import mysql.connector as mariadb
+import sys
+import urllib.request, json
 import datetime
 import time
-
-with open("config.yaml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.BaseLoader)
-
-    mariadb_connection = mariadb.connect(
-        host=cfg['mysql']['host'],
-        port=cfg['mysql']['port'],
-        user=cfg['mysql']['user'],
-        password=cfg['mysql']['passwd'],
-        database='mysql'
-    )
-    cursor = mariadb_connection.cursor()
-
-    mariadb_connection.close()
 
 # Define Song class
 class Song:
@@ -46,13 +34,42 @@ class Song:
             return NotImplemented
         return self.title == other.title and self.interpret == other.interpret and self.playtime == other.playtime
 
+if os.path.isfile('config.yaml'):
+    with open('config.yaml', 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.BaseLoader)
+else:
+    print('config.yaml does not exist. Save config-sample.yaml as config.yaml and adapt the file.')
+    sys.exit(1)
 
+# Instantiate Connection
+try:
+    conn = mariadb.connect(
+        host=cfg['mysql']['host'],
+        port=cfg['mysql']['port'],
+        user=cfg['mysql']['user'],
+        password=cfg['mysql']['passwd']
+    )
+except mariadb.Error as e:
+    print('Error connecting to MariaDB Platform: {e}')
+    sys.exit(1)
 
-s1 = Song("Shake it off", "Taylor Swift", "00:00", "", "2020-03-17T10:14:55+01:00",
-          "s",
-          "s"
-          )
-"""
+# Instantiate Cursor
+cur = conn.cursor()
+
+# Create DB and select it
+cur.execute('CREATE DATABASE IF NOT EXISTS ' + cfg['mysql']['db'])
+conn.connect(database=cfg['mysql']['db'])
+
+# Create table
+cur.execute('CREATE TABLE IF NOT EXISTS songs ('
+            'title TINYTEXT, '
+            'interpret TINYTEXT, '
+            'duration TIME(0), '
+            'label TINYTEXT, '
+            'playtime DATETIME(0), '
+            'm3uURL TINYTEXT, '
+            'imageURL TINYTEXT);')
+
 while True:
     try:
         with urllib.request.urlopen(cfg['pilatus']['url']) as url:
@@ -61,17 +78,29 @@ while True:
             currentSong = Song(
                 currentSong['live'][0]['title'],
                 currentSong['live'][0]['interpret'],
-                currentSong['live'][0]['duration'],
+                time.strptime(currentSong['live'][0]['duration'], '%M:%S'),
                 currentSong['live'][0]['label'],
                 datetime.datetime.fromisoformat(currentSong['live'][0]['playtime']),
                 currentSong['live'][0]['m3uURL'],
                 currentSong['live'][0]['imageURL']
             )
             print(currentSong)
-
-            time.sleep(5)
+            cur.execute('INSERT INTO songs (title, interpret, duration, label, playtime, m3uURL, imageURL) VALUES("'
+                        + currentSong.title + '", "'
+                        + currentSong.interpret + '", "'
+                        + time.strftime('%M:%S', currentSong.duration) + '", "'
+                        + currentSong.label + '", "'
+                        + str(datetime.datetime.strftime(currentSong.playtime, '%Y-%m-%d %H:%M:%S')) + '", "'
+                        + currentSong.m3uURL + '", "'
+                        + currentSong.imageURL
+                        + '");'
+                        )
+            conn.commit()
+            time.sleep(60)
     except urllib.error.HTTPError as e:
         print(e.__dict__)
     except urllib.error.URLError as e:
         print(e.__dict__)
-"""
+
+# Close Connection
+conn.close()
